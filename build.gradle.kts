9b766a5adfb52c0d0ec7d93a83fa9c762ee16fe8
ksyoo <ksyoo@nextree.io>
2023-08-04 17:31:13 +0900

    ✨ DiscardServer 구현

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("java")
 }
 
-group = "org.example"
+group = "com.hello"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -10,6 +10,9 @@ repositories {
 }
 
 dependencies {
+    // https://mvnrepository.com/artifact/io.netty/netty-all
+    implementation("io.netty:netty-all:4.1.96.Final")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
